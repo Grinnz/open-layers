9818d4755149ee3aae798661de46c5e4b35719ae
Dan Book <grinnz@grinnz.com>
2020-02-14 11:54:15 -0500

    initial commit

---
+++ lib/open/layers.pm
@@ -0,0 +1,35 @@
+package open::layers;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+open::layers - Module abstract
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Report any issues on the public bugtracker.
+
+=head1 AUTHOR
+
+Dan Book <dbook@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2020 by Dan Book.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=head1 SEE ALSO
+
