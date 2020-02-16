09e5f1e3ca3355578dfeefd7ef57a206226b79f6
Dan Book <grinnz@grinnz.com>
2020-02-16 15:20:38 -0500

    doc updates

--- lib/open/layers.pm
+++ lib/open/layers.pm
@@ -44,27 +44,27 @@ sub import {
 
 =head1 NAME
 
-open::layers - Set default PerlIO layers, or set PerlIO layers on standard
-filehandles
+open::layers - Set default PerlIO layers
 
 =head1 SYNOPSIS
-
-  # set layers on the standard handles (not lexical)
-  use open::layers STDIN => ':encoding(UTF-8)';
-  use open::layers STDOUT => ':encoding(UTF-8)', STDERR => ':encoding(UTF-8)';
-  use open::layers STDIO => ':encoding(UTF-8)'; # shortcut for all of above
 
   # set default for open() in this lexical scope
   use open::layers r => ':raw';
   use open::layers r => ':encoding(cp1252)', w => ':encoding(UTF-8)';
   use open::layers rw => ':raw:crlf'; # all opened handles
 
+  # set layers on the standard handles (not lexical)
+  use open::layers STDIN => ':encoding(UTF-8)';
+  use open::layers STDOUT => ':encoding(UTF-8)', STDERR => ':encoding(UTF-8)';
+  use open::layers STDIO => ':encoding(UTF-8)'; # shortcut for all of above
+
 =head1 DESCRIPTION
 
 This pragma is a reimagination of the core L<open> pragma, which either sets
 L<PerlIO> layers on the global standard handles, or sets default L<PerlIO>
 layers for handles opened in the current lexical scope. The interface is
-redesigned to be more explicit and intuitive.
+redesigned to be more explicit and intuitive. See L</"COMPARISON TO open.pm>
+for details.
 
 =head1 ARGUMENTS
 
@@ -110,27 +110,37 @@ The second argument is the layer or layers to set. Multiple layers can be
 specified like C<:foo:bar>, as in L<open()|perlfunc/open> or
 L<binmode()|perlfunc/binmode>.
 
-=head1 COMPARISON TO OPEN.PM
+=head1 COMPARISON TO open.pm
 
 =over
 
 =item *
 
-Unlike C<open.pm>, C<open::layers> requires that the target of the operation is
+Unlike L<open>, C<open::layers> requires that the target of the operation is
 always specified so as to not confuse global and lexical operations.
 
 =item *
 
-Unlike C<open.pm>, C<open::layers> can set layers on the standard handles
-without affecting handles opened in the lexical scope.
+Unlike L<open>, C<open::layers> can set layers on the standard handles without
+affecting handles opened in the lexical scope.
 
 =item *
 
-Unlike C<open.pm>, multiple layers are not required to be space separated.
+Unlike L<open>, multiple layers are not required to be space separated.
 
 =item *
 
-Unlike C<open.pm>, the C<:locale> pseudo-layer is not (yet) implemented.
+Unlike L<open>, the C<:locale> pseudo-layer is not (yet) implemented.
+
+=item *
+
+Unlike L<open>, duplicate existing encoding layers are not removed from the
+standard handles. Either ensure that nothing else is setting encoding layers on
+these handles, or use the C<:raw> pseudo-layer to "reset" the layers to a
+binary stream before applying text translation layers.
+
+  use open::layers STDIO => ':raw:encoding(UTF-16BE)';
+  use open::layers STDIO => ':raw:encoding(UTF-16BE):crlf'; # on Windows
 
 =back
 
